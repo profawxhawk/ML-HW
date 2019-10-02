cfdb693382b7200296973f8de4029cb834f61c50
profawxhawk <bharath17035@iiitd.ac.in>
2019-10-02 22:32:56 +0530

    Q1 completed

--- ml-hw-2/q1.py
+++ ml-hw-2/q1.py
@@ -1,9 +1,13 @@
 import h5py
 import numpy as np
+import pandas as pd
+from scipy import stats
 from svm import SVM
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
-def ker_d1(X,Y):
+import warnings
+warnings.filterwarnings("ignore")
+def ker_1(X,Y):
     return np.square(np.dot(X, Y.T))
 def ker_2(X,Y):
     gram_matrix = np.zeros((X.shape[0],Y.shape[0]))
@@ -22,7 +26,7 @@ def fit_and_contour(X,Y,myker):
         else:
             cols.append('blue')
     plt.scatter(X[:,0], X[:,1],c=cols)
-    svc= SVC(kernel='rbf')
+    svc= SVC(kernel=myker)
     svc.fit(X,Y.ravel())
     # Get limit of axis to form meshgrid
     ax = plt.gca()
@@ -37,13 +41,8 @@ def fit_and_contour(X,Y,myker):
     if myker==ker_3:
         Z = svc.predict(np.c_[xm1.ravel(), ym1.ravel()])
         Z = Z.reshape(xm1.shape)
-        plt.pcolormesh(xm1, ym1, Z, cmap=plt.cm.Paired)
-
-        # Plot also the training points
-        plt.scatter(X[:, 0], X[:, 1],c=cols, edgecolors='k')
-        plt.title('3-Class classification using Support Vector Machine with custom'
-                ' kernel')
-        plt.axis('tight')
+        plt.contourf(xm1, ym1, Z > 0,alpha=0.4)
+        ax.contour(xm1, ym1, Z, colors='r', levels=[-1, 0, 1],linestyles=['--', '-', '--'])
         plt.show()
     else:
         Z = svc.decision_function(xy).reshape(xm1.shape)
@@ -74,27 +73,101 @@ def extract_h5(file_name):
         else:
             data.append(np.asarray(group[()]))
     return data
+
+def iqr(da):
+    o=pd.DataFrame(da)
+    Q1 = o.quantile(0.25)
+    Q3 = o.quantile(0.75)
+    IQR = Q3 - Q1
+    out = o[~((o < (Q1 - 0.1 * IQR)) |(o > (Q3 + 0.1 * IQR))).any(axis=1)]
+    return out
+
+def z_score(temp,kernel):
+    z = np.abs(stats.zscore(temp[1]))
+    k = temp[1][(z < 1.5).all(axis=1)]
+    z1 = np.abs(stats.zscore(temp[-1]))
+    k1 = temp[-1][(z1 < 1.5).all(axis=1)]
+    input=np.append(k,k1,axis=0)
+    print(input.shape)
+    output=[1]*len(k)
+    output0=[0]*len(k1)
+    output=output+(output0)
+    output=np.asarray(output).reshape(input.shape[0],1)
+    print(output.shape)
+    data=[input,output]
+    fit_and_contour(data[0],data[1],kernel)
+        
+    
+def seg(X,Y):
+    ones=[]
+    zeros=[]
+    for i,v in enumerate(Y):
+        if v==0:
+            zeros.append(X[i])
+        else:
+            ones.append(X[i])
+            
+    return {-1:np.array(zeros), 1:np.array(ones)}
+
 def split_data(data):
     split=(int)(4*(len(data[1]))/5)
     x1=np.array(data[0])
     y1=np.array(data[1])
     temp=np.column_stack((x1,y1))
     return (temp[0:split],temp[split:])
+
+def outlier_remove(index,data):
+    if index==2:
+        kernel=ker_3
+    else:
+        kernel='rbf'
+    svc= SVC(kernel='rbf')
+    X=data[0]
+    Y=data[1]
+    svc.fit(X,Y.ravel())
+    data_woo=[]
+    output_woo=[]
+    for (i,j) in zip(X,Y):
+        ans=svc.predict([i])
+        if ans==-1:
+            ans=0
+        if ans==j:
+            data_woo.append(i)
+            output_woo.append(j)
+            
+    seg_data=seg(data_woo,output_woo)   
+    z_score(seg_data,kernel)
+    
 if (__name__ == "__main__"):
     data=[]
     for i in range(1,6):
         data.append(extract_h5('./q1_datasets/data_'+str(i)+'.h5'))
-    # for i in data:
-        # ploty(i[0],i[1])
-    #fit_and_contour(data[0][0],data[0][1],ker_1)
-    #fit_and_contour(data[1][0],data[1][1],ker_2)
-    #fit_and_contour(data[2][0],data[2][1],ker_3)
-    #fit_and_contour(data[3][0],data[3][1],'rbf')
-    #fit_and_contour(data[4][0],data[4][1],'rbf')
-    # for i in range(3,5):
-    #     train,test=split_data(data[i])
-    #     svm_model=SVM(train,test)
-    #     svm_model.linear_svm()
+    print("Data points scatter plot")
+    for i in data:
+        ploty(i[0],i[1])
+    print("Decision boundaries for datasets")
+    fit_and_contour(data[0][0],data[0][1],ker_1)
+    fit_and_contour(data[1][0],data[1][1],ker_2)
+    fit_and_contour(data[2][0],data[2][1],ker_3)
+    fit_and_contour(data[3][0],data[3][1],'rbf')
+    fit_and_contour(data[4][0],data[4][1],'rbf')
     
+    print("Outlier removed data")
+    for i,j in enumerate(data):
+        outlier_remove(i,j)
+
+    print("Linear svm")
+    print()
+    for i in range(3,5):
+        train,test=split_data(data[i])
+        svm_model=SVM(train,test)
+        svm_model.linear_svm()
+    
+    print("RBF svm")
+    print()
+    for i in range(3,5):
+        train,test=split_data(data[i])
+        svm_model=SVM(train,test)
+        svm_model.rbf_svm()
     
                 
--- ml-hw-2/svm.py
+++ ml-hw-2/svm.py
@@ -8,6 +8,34 @@ class SVM:
     def __init__(self,train,test):
         self.train=train
         self.test=test
+    
+    def predict_rbf(self,svc,x):
+        s_v=svc.support_vectors_
+        coef=svc.dual_coef_[0]
+        base=svc.intercept_
+        pred=[]
+        for i in range(len(x)):
+            temp_in=x[i]
+            ans=0
+            for i in range(len(coef)):
+                temp=coef[i]
+                ans+=(temp*np.exp(-0.6*np.linalg.norm(s_v[i]-temp_in)**2))
+            ans+=base
+            if(ans>=0):
+                    pred.append(1)
+            else:
+                    pred.append(0)
+    
+        return np.asarray(pred)
+    def rbf_svm(self):
+        svc= SVC(kernel='rbf')
+        svc.fit(self.train[:,0:2],self.train[:,2].ravel())
+        y_pred=svc.predict(self.test[:,0:2])
+        y_pred1=self.predict_rbf(svc,self.test[:,0:2])
+        print('Accuracy score from SVM predict')
+        self.accuracy(y_pred)
+        print('Accuracy score from self defined predict')
+        self.accuracy(y_pred1)
         
     def linear_svm(self):
         svc= SVC(kernel='linear')
@@ -19,8 +47,10 @@ class SVM:
         self.accuracy(y_pred)
         print('Accuracy score from self defined predict')
         self.accuracy(y_pred1)
+        
     def accuracy(self,pred):
         print ('Accuracy Score :',accuracy_score(self.test[:,2].ravel(),pred))
+    
     def fit(self, data):
         pass
     
