1504b707138dd0d3348a80211984f378c50e4b55
profawxhawk <bharath17035@iiitd.ac.in>
2019-10-10 17:27:12 +0530

    done

--- ml-hw-2/q2.py
+++ ml-hw-2/q2.py
@@ -33,7 +33,7 @@ def convert(data):
 
 def extract_data():
     train_data=[]
-    for i in range(1,6):
+    for i in range(1,2):
         file='./cifar-10-batches-py/data_batch_'+str(i)
         temp=unpickle(file)
         temp_data=[]
@@ -44,28 +44,17 @@ def extract_data():
         temp_data.append(np.asarray(mat_data).astype('float64'))
         temp_data.append(np.asarray(temp[b'labels']).reshape(temp[b'data'].shape[0],1))
         train_data.append(temp_data)
-    temp=unpickle('./cifar-10-batches-py/test_batch')
-    temp_data=[]
-    mat_data=[]
-    for j in range(len(temp[b'data'])):
-        op=convert(temp[b'data'][j])
-        mat_data.append(op)
-    temp_data.append(np.asarray(mat_data).astype('float64'))
-    temp_data.append(np.asarray(temp[b'labels']).reshape(temp[b'data'].shape[0],1))
-    test_data=temp_data
     temp=unpickle('./cifar-10-batches-py/batches.meta')
     labels=temp[b'label_names']
     for i in range(len(labels)):
         labels[i]=(labels[i].decode("utf-8"))
-    return train_data,test_data,labels
+    return train_data,labels
 
-def normalize123(data,test):
+def normalize123(data):
     data=data.astype('float64')
-    test=test.astype('float64')
     mean = np.mean(data, axis = 0)
     data -= mean
-    test -= mean
-    return data,test
+    return data
 
 def roc(ovo,X_test,Y_test):
     y = label_binarize(Y_test, classes=[0,1,2,3,4,5,6,7,8,9])
@@ -98,6 +87,7 @@ def svm_linear(X,Y,x,y,model):
     results = confusion_matrix(Y_test.ravel(),y_pred) 
     print("Confusion Matrix: ",results) 
     roc(mod,X_test,Y_test)
+    print(mod.estimators_[0].coef_)
 
 def svm_rbf(X,Y,x,y,model):
     X_train=X
@@ -116,6 +106,7 @@ def svm_rbf(X,Y,x,y,model):
     results = confusion_matrix(Y_test.ravel(),y_pred) 
     print("Confusion Matrix: ",results) 
     roc(mod,X_test,Y_test)
+    print(mod.estimators_[0].dual_coef_)
     
 def svm_poly(X,Y,x,y,model):
     X_train=X
@@ -134,6 +125,8 @@ def svm_poly(X,Y,x,y,model):
     results = confusion_matrix(Y_test.ravel(),y_pred) 
     print("Confusion Matrix: ",results) 
     roc(mod,X_test,Y_test)
+    print(mod.estimators_[0].dual_coef_)
+    
 def pca(train):
     pca = PCA(n_components=100, random_state=0, svd_solver='randomized')
     pca.fit(train)
@@ -156,45 +149,95 @@ def change_label(index,input):
 def one_vs_all(ker,TrainX,TrainY,TestX,TestY):
     fpr_li=[]
     tpr_li=[]
+    values=[]
     for i in range(10):
-        mod=SVC(kernel=ker)
+        mod=SVC(kernel=ker, probability=True)
         temp_trainy=change_label(i,TrainY)
         temp_testy=change_label(i,TestY)
         mod.fit(TrainX,temp_trainy.ravel())
-        probs = mod.predict_proba(TestX)
-        probs = probs[:, 1]
+        probs = mod.decision_function(TestX)
         fpr, tpr, thresholds = roc_curve(temp_testy, probs)
         fpr_li.append(fpr)
         tpr_li.append(tpr)
         y_pred = mod.predict(TestX)
-        accuracy = accuracy_score(TestY.ravel(), y_pred)
-        print("Model accuracy is: ", accuracy)
-        results = confusion_matrix(TestY.ravel(),y_pred) 
-        print("Confusion Matrix: ",results) 
+        values.append(mod.predict_proba(TestX)[:,1])
+    predict=[]
+    for j in range(2000):
+        max_val=values[0][j]
+        index=0
+        for i in range(1,10):
+            if(values[i][j]>max_val):
+                max_val=values[i][j]
+                index=i
+        predict.append(index)
+    predict=np.asarray(predict)  
+    accuracy = accuracy_score(TestY,predict)
+    print("Model accuracy is: ", accuracy)
+    results = confusion_matrix(TestY,predict) 
+    print("Confusion Matrix: ",results) 
     for i in range(len(fpr_li)):
         plt.plot(fpr_li[i],tpr_li[i],label=('Class for '+str(i)))
     plt.legend(loc='lower right')
     plt.show()
+def find_pair(TrainX,TrainY,i,j):
+    X=[]
+    Y=[]
+    for k in range(len(TrainY)):
+        if TrainY[k]==[i] or TrainY[k]==[j]:
+            X.append(TrainX[k])
+            Y.append(TrainY[k])
+    return np.asarray(X),np.asarray(Y)
+            
+def one_vs_one(ker,TrainX,TrainY,TestX,TestY):
+    values=[]
+    for i in range(10):
+        for j in range(i+1,10):
+            X,Y=find_pair(TrainX,TrainY,i,j)
+            mod=SVC(kernel=ker, probability=True)
+            mod.fit(X,Y.ravel())
+            values.append(mod.predict(TestX))
+    predict=[]
+    for j in range(2000):
+        max_count=[0]*10
+        for i in range(len(values)):
+            max_count[values[i][j]]=max_count[values[i][j]]+1
+        temp=-10
+        index=0
+        for k in range(10):
+            if max_count[k]>temp:
+                temp=max_count[k]
+                index=k
+        predict.append(index)
+    predict=np.asarray(predict)
+    accuracy = accuracy_score(TestY,predict)
+    print("Model accuracy is: ", accuracy)
+    results = confusion_matrix(TestY,predict) 
+    print("Confusion Matrix: ",results) 
     
 def call_svm(X,Y,x,y):
     print("linear svm ovo start")
     svm_linear(X,Y,x,y,'ovo')
+    # one_vs_one('linear',X,Y,x,y)
     print("linear svm ovr start")
     svm_linear(X,Y,x,y,'ovr')
+    # one_vs_all('linear',X,Y,x,y)
     print("rbf svm ovo start")
     svm_rbf(X,Y,x,y,'ovo')
+    # one_vs_one('rbf',X,Y,x,y)
     print("rbf svm ovr start")
     svm_rbf(X,Y,x,y,'ovr')
+    # one_vs_all('rbf',X,Y,x,y)
     print("poly svm ovo start")
     svm_poly(X,Y,x,y,'ovo')
+    # one_vs_one('poly',X,Y,x,y)
     print("poly svm ovr start")
     svm_poly(X,Y,x,y,'ovr')
+    # one_vs_all('poly',X,Y,x,y)
 
 if (__name__ == "__main__"):
-    train_data,test_data,labels=extract_data()
+    train_data,labels=extract_data()
     for i in train_data:
-        temp_test=test_data
-        i[0],temp_test[0]=normalize123(i[0],temp_test[0])
+        i[0]=normalize123(i[0])
         i[0]=pca_proc(i[0])
     k_cross_split=split_train(train_data[0])
     
